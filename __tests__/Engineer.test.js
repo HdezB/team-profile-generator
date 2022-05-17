4d1c1759ce1f8b1efac2437bc2a22b588403972a
DukeHdez <briayan27@outlook.com>
2022-05-17 16:58:13 -0400

    Add engineer class

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,18 @@
+const Engineer = require('../lib/Engineer');
+
+test('create engineer object', () => {
+    const engineer = new Engineer('John', '00001', 'hellothere@obi-wan.com', 'johnGit');
+
+    expect(engineer.name).toBe('John');
+    expect(engineer.id).toBe('00001');
+    expect(engineer.email).toBe('hellothere@obi-wan.com');
+    expect(engineer.gitUsername).toBe('johnGit');
+}) 
+test('test the getName(), getId(), getEmail, getRole and getGitUsername methods', () => {
+    const engineer = new Engineer('John', '00001', 'hellothere@obi-wan.com', 'johnGit');
+
+    expect(engineer.getName()).toBe('John');
+    expect(engineer.getId()).toBe('00001');
+    expect(engineer.getEmail()).toBe('hellothere@obi-wan.com');
+    expect(engineer.getGitUsername()).toBe('johnGit');
+}) 
